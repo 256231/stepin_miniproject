b74695e6a6c8896bb3b91f64f6d98b94ef33f081
Himanshu <himanshu.kaushish14@gmail.com>
2021-04-13 13:50:40 +0530

    test code edited

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -1,3 +1,14 @@
+
+/**
+ * @file test.c
+ * @author Himanshu
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-13
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include"unity.h"
 #include"unity_internals.h"
 #include"electrical.h"
@@ -6,6 +17,10 @@ void setUp(){}
 /* Required by the unity test framework */
 void tearDown(){}
 
+/**
+ * @brief Transformer test cases
+ * 
+ */
 void test_transformer()
 {
   TEST_ASSERT_EQUAL(SUCCESS,transformer_tests(220,1.25,150,20,12.5));
@@ -13,6 +28,11 @@ void test_transformer()
   TEST_ASSERT_EQUAL(ERROR,transformer_tests(20,1.25,150,300,12.5));
   TEST_ASSERT_EQUAL(ERROR,transformer_tests(220,20,150,300,12.5));
 }
+
+/**
+ * @brief Motor test cases
+ * 
+ */
 void test_motor()
 {
   TEST_ASSERT_EQUAL(SUCCESS,motor_tests(1,2));
